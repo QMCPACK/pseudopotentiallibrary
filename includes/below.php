df263107826fe41521253e520286f850419f2e0d
Paul R. C. Kent <kentpr@ornl.gov>
2018-02-13 23:39:16 -0500

    Test below update

--- includes/below.php
+++ includes/below.php
@@ -1,1 +1,8 @@
 <!-- Markdown/HTML/Text can go here to be shown below the recipes -->
+# Help and feedback
+
+Create an issue on the [GitHub site](https://github.com/QMCPACK/pseudopotentiallibrary) or contact the developers of the potential you are interested in.
+
+# Contributing
+
+Create a pull request on the [GitHub site](https://github.com/QMCPACK/pseudopotentiallibrary) including details of the published papers or preprints where the potentials will be fully described.
