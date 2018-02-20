8626e0c0478e66bc26fd9ff6d6eba483833d9ef2
Paul Kent <kentpr@ornl.gov>
2018-02-20 17:16:00 -0500

    Improve text

--- includes/below.php
+++ includes/below.php
@@ -1,8 +1,12 @@
 <!-- Markdown/HTML/Text can go here to be shown below the recipes -->
 # Help and feedback
 
-Create an issue on the [GitHub site](https://github.com/QMCPACK/pseudopotentiallibrary) or contact the developers of the potential you are interested in.
+Create an issue on the [GitHub site](https://github.com/QMCPACK/pseudopotentiallibrary/issues) or contact the developers of the potential you are interested in.
 
 # Contributing
 
-Create a pull request on the [GitHub site](https://github.com/QMCPACK/pseudopotentiallibrary) including details of the published papers or preprints where the potentials will be fully described.
+Thanks for contributing! Please create a pull request on the [GitHub site](https://github.com/QMCPACK/pseudopotentiallibrary/pulls) from a forked and branched copy of the repo. Please include details of the published papers or preprints where the potentials will be fully described.  [Create an issue](https://github.com/QMCPACK/pseudopotentiallibrary/issues) for help with this or to discuss a contribution. 
+
+# News and Major Updates
+
+* 2017-02-20 Initial public version with ccECPs only.
