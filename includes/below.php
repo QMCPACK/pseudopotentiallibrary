8ceb9858f22dcb92b7845fddc2decc8e80018dda
Paul R. C. Kent <kentpr@ornl.gov>
2018-03-23 17:27:34 -0400

    Update below.php

--- includes/below.php
+++ includes/below.php
@@ -12,7 +12,14 @@ Create an issue on the [GitHub site](https://github.com/QMCPACK/pseudopotentiall
 
 Thanks for contributing! Please create a pull request on the [GitHub site](https://github.com/QMCPACK/pseudopotentiallibrary/pulls) from a forked and branched copy of the repo. Please include details of the published papers or preprints where the potentials will be fully described.  [Create an issue](https://github.com/QMCPACK/pseudopotentiallibrary/issues) for help with this or to discuss a contribution. 
 
+## Roadmap
+
+* 2018-04 Add newly developed 2nd and 3rd row ccECPs
+* 2018-04 Add Trail-Needs 2017 potentials
+* Standardize naming of basis sets for different codes
+* Convert or create new formats where missing, e.g. UGAMESS, Molpro, or UPF format with projectors for Quantum Espresso.
+
 ## News and Major Updates
 
-* 2017-02-22 Add RRKJ and TM potentials for 3d elements.
-* 2017-02-20 Initial public version with ccECPs.
+* 2018-02-22 Add RRKJ and TM potentials for 3d elements.
+* 2018-02-20 Initial public version with ccECPs.
